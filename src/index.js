3489c0ba6557a21d52d3e8c5e72439d1fa87833a
aehmeve <ehmorizehere@gmail.com>
2024-07-30 04:34:25 +0900

    feat: solve task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,37 @@
 module.exports = function toReadable (number) {
+  const TEN = [
+    'ten', 'eleven', 'twelve', 'thirteen', 'fourteen', 'fifteen',
+    'sixteen', 'seventeen', 'eighteen', 'nineteen'
+  ]
+  const TEN_PLUS = [
+    '', '', 'twenty', 'thirty', 'forty', 'fifty', 'sixty', 'seventy', 'eighty',
+    'ninety'
+  ]
+  const UNIT = [
+    '', 'one', 'two', 'three', 'four', 'five', 'six', 'seven', 'eight', 'nine'
+  ]
+  const arrNumber = number.toString().split('').reverse();
+  let hundreds = '';
+  let tens = '';
+  let units = '';
   
+  if(number === 0) {
+  	return 'zero';
+  }
+  
+  if (+arrNumber[1] === 1) {
+  	tens = `${TEN[+arrNumber[0]]}`
+  } else {
+  	if (arrNumber[1]) {
+    	tens = `${TEN_PLUS[+arrNumber[1]]}`
+    }
+    units = `${UNIT[+arrNumber[0]]}`
+  }
+  if (arrNumber[2]) {
+  	hundreds = `${UNIT[+arrNumber[2]]} hundred`
+  }
+  
+  let result = [hundreds, tens, units].filter((word) => word !== '').join(' ');
+  
+  return result;
 }
